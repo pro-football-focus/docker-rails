992385abcd4041a4fc384e95cecf62c4067df0b6
Brad Condo <bradcondo@gmail.com>
2016-11-11 12:28:44 -0500

    Added passenger+nginx configuration

--- docker-app.sh
+++ docker-app.sh
@@ -13,4 +13,4 @@ fi
 # Setup and start the rails application
 cd /app; RAILS_ENV=$RAILS_ENV; bundle install && \
                                bundle exec rake db:migrate && \
-                               bundle exec rails s -p 3000 -b '0.0.0.0' -e $RAILS_ENV
+                               nginx
